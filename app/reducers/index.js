ff60ebd9bd82d1423860c5906c7331a11f13bedd
Linda Saraguro <lindasaraguro@yahoo.com>
2019-02-11 16:46:27 -0500

    created a combineReducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,52 +1,16 @@
 // `combineReducers` is not currently being used...but it should!
 // When you're ready to use it, un-comment the line below!
 
-// import {combineReducers} from 'redux'
+import {combineReducers} from 'redux'
+import studentSubReducer from './subStudentReducer'
+import campusSubReducer from './subCampusReducer'
 
-const initialState = {}
-
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state
-  }
-}
+const rootReducer = combineReducers({
+  studentSubReducer,
+  campusSubReducer
+})
 
 export default rootReducer
 
-// Syntax example!!!!!
 
-// import Axios from 'axios'
-
-// //action types
-// const GET_CANDIES = 'GET_CANDIES'
-
-// //action creators
-// export const getCandies = (candies) => ({
-//   type: GET_CANDIES,
-//   candies: candies
-// })
-
-// //thunk creators
-// export const fetchCandies = () => {
-//     return async dispatch => {
-//     const response = await Axios.get('/api/candies')
-//     dispatch(getCandies(response.data))
-//   }
-// }
-
-// //inital state
-// const initialState = {
-//   candies: []
-// }
-
-// const rootReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case GET_CANDIES:
-//     return {...state, candies: action.candies}
-//     default:
-//       return state
-//   }
-// }
-
-// export default rootReducer
+//double check if this syntax for combine-reducer is correct
