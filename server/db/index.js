3cce1019aa025eaf3e531122fad8744fecfeb4a5
Linda Saraguro <lindasaraguro@yahoo.com>
2019-02-11 10:56:51 -0500

    completed campuses and students models

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,66 @@
 'use strict'
-
+const Sequelize = require('sequelize')
 const db = require('./database')
+
+db.define('campus', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'placeholder for now'
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+})
+
+db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'placeholder for now'
+  },
+  gpa: {
+    type: Sequelize.DECIMAL,
+    validate: {
+      min: 0.0,
+      max: 4.0
+    }
+  }
+})
 
 // The purpose of this module is to bring your Sequelize instance (`db`) together
 // with your models (which you should define in separate modules in this directory).
@@ -15,7 +75,13 @@ const db = require('./database')
 //
 // Puppy.belongsTo(Owner)
 
+
 module.exports = {
   // Include your models in this exports object as well!
   db
 }
+
+// QUESTIONS: 
+// what do i need to add in th exports?
+// find placehplder for image url
+//do we need commas after each column?
